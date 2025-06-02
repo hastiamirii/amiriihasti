c8388f82c0c4d93b1a77552119f53bdd7fc1f7cc
hastiamirii <ahasti887@gmail.com>
2025-06-02 16:19:20 +0330

    1

---
+++ New folder (2)/New folder (2)/qgis2web_20/data/basandtaxi2_5.js
@@ -0,0 +1,9 @@
+var json_basandtaxi2_5 = {
+"type": "FeatureCollection",
+"name": "basandtaxi2_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "way\/245380300", "@id": "way\/245380300", "addr_city": "قائمشهر", "addr_stree": "یاس ۶ خیابان مبارزان", "amenity": "taxi", "name": "ایستگاه سواری ساری", "bus": null, "highway": null, "public_tra": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 52.8611376, 36.4638484 ], [ 52.8615748, 36.4641133 ], [ 52.8616769, 36.4639914 ], [ 52.8612455, 36.4637453 ], [ 52.8611376, 36.4638484 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": "way\/479576931", "@id": "way\/479576931", "addr_city": "قائم شهر", "addr_stree": "خیابان صالحی مازندرانی", "amenity": "taxi", "name": "ایستگاه سواری قائمشهر-سوادکوه", "bus": null, "highway": null, "public_tra": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 52.8548141, 36.4593168 ], [ 52.8552934, 36.4590963 ], [ 52.8551842, 36.4589474 ], [ 52.8547338, 36.4591806 ], [ 52.8548141, 36.4593168 ] ] ] ] } }
+]
+}
---
+++ New folder (2)/New folder (2)/qgis2web_20/data/farmandarii_26.js
@@ -0,0 +1,8 @@
+var json_farmandarii_26 = {
+"type": "FeatureCollection",
+"name": "farmandarii_26",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 52.874165999827895, 36.470399071210544 ] } }
+]
+}
